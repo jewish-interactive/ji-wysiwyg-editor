570da3f148fa7dc921e32163660ea98b87e1d4b4
jpuri <jyotipuri@gmail.com>
2019-01-05 22:22:57 +0530

    Initial commit

---
+++ stories/index.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import { Editor } from "../index";
+
+storiesOf("JI Wysiwyg Editor", module).add("simple editor", () => <Editor />);
