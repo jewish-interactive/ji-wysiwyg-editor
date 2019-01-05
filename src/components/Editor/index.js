4faaef9120b1d0ea3e07a37af999e2cddec04f3f
jpuri <jyotipuri@gmail.com>
2019-01-05 22:32:16 +0530

    Removing toolbar options not needed.

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -2,4 +2,11 @@ import React from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-export default () => <Editor />;
+const TOOLBAR_CONFIG = {
+  options: ["inline", "textAlign", "colorPicker", "fontFamily", "fontSize"],
+  inline: {
+    options: ["bold", "italic", "underline"]
+  }
+};
+
+export default () => <Editor toolbar={TOOLBAR_CONFIG} />;
